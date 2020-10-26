da6b1174a8672c99e0b0ddde7566eac4a2cbca2a
Nestor Lemus <nestorjlemus@gmail.com>
2020-10-26 03:46:26 -0600

    Proyecto Examen

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_77UOK39qI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5bsif04aipp1r107squg0lu2rk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://pmbdg2p9vf.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
